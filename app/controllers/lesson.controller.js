337fd365eb30ad8f11bac957620ac23f5bc07f17
Nadi Duno <nadiduno@gmail.com>
2023-09-28 14:47:25 -0300

    Retrive All Lesson

--- app/controllers/lesson.controller.js
+++ app/controllers/lesson.controller.js
@@ -27,3 +27,20 @@ exports.create = (req, res) => {
             });
         });
 };
+
+// Retrieve all Lessons from the database.
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
+
+    Lesson.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving lesson."
+            });
+        });
+};
