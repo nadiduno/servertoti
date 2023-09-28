cd6c7c9af364acdf20b27dba839546caa8d243b9
Nadi Duno <nadiduno@gmail.com>
2023-09-28 14:41:32 -0300

    Create Lesson

--- app/routes/lesson.routes.js
+++ app/routes/lesson.routes.js
@@ -12,4 +12,36 @@ module.exports = app => {
     // }
     next();
   });
+
+  // Create a new Lesson
+  router.post("/",
+
+    // Validando com express-validator
+
+    body('title')
+      .notEmpty().isString().isLength({ min: 3 }).withMessage('O título da aula deve ter pelo menos 3 caracteres'),
+
+    body('description')
+      .notEmpty().isString().isLength({ min: 3 }).withMessage('A descripção da aula deve ter pelo menos 3 caracteres'),
+
+    body('teacher')
+      .notEmpty().isString().isLength({ min: 3 }).withMessage('O nome do Professor deve ter pelo menos 3 caracteres'),
+
+    body('typelesson')
+      .notEmpty().isString().isLength({ min: 3 }).withMessage('O tipo da aula deve ter pelo menos 3 caracteres'),
+
+    body('linkteste')
+      .optional().isString().isURL().matches(/docs\.google\.com/).withMessage('O URL da avaliação deve ser uma url válida, deve ser de um formulário do Google Form'),
+
+    body('published')
+      .notEmpty().isBoolean().withMessage('O campo de publicado não deve ser vazio, deve ser True ou False'),
+
+
+    (req, res) => {
+      const result = validationResult(req);
+      result.errors.length ? returnError(result.errors, res) : lessons.create(req, res);
+    });
+
+
+
 };
